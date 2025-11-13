03c2c79d3867ad42468e7892090742fb703ae35b
Felipe Scalco <fscalco7@gmail.com>
2025-11-13 17:34:34 -0300

    feat [WEB]: register and login user routes

--- frontend/src/api/amiibo-api.js
+++ frontend/src/api/amiibo-api.js
@@ -1,15 +1,37 @@
 export class AmiiboApi {
-    apiUrl = 'https://www.amiiboapi.com/api/amiibo/';
+    apiUrl = 'http://localhost:4000';
 
-    async getAmiibos() {
-        let amiibos = await fetch(this.apiUrl);
-        let amiiboJson = await amiibos.json();
-        return amiiboJson.amiibo;
+    // async getAmiibos() {
+    //     let amiibos = await fetch(this.apiUrl);
+    //     let amiiboJson = await amiibos.json();
+    //     return amiiboJson.amiibo;
+    // }
+
+    // async searchAmiibos(name) {
+    //     let amiibos = await fetch(`${this.apiUrl}?name=${name}`);
+    //     let amiiboJson = await amiibos.json();
+    //     return amiiboJson.amiibo;
+    // }
+
+    async login(username, password){
+        const response = await fetch(`${this.apiUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+        return await response.json();
     }
 
-    async searchAmiibos(name) {
-        let amiibos = await fetch(`${this.apiUrl}?name=${name}`);
-        let amiiboJson = await amiibos.json();
-        return amiiboJson.amiibo;
+    async registerUser(username, password){
+        let response = await fetch(`${this.apiUrl}/api/auth/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+        return await response.json();
     }
 }
